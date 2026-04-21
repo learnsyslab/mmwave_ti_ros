eee2d47fe0cb8ed73b61a9e905c1e3e63f03aba9
Haoming <haoming.zhang@rwth-aachen.de>
2026-04-21 11:31:09 +0200

    added saberguide config

--- ti_mmwave_rospkg/launch/6843AOP_odom.py
+++ ti_mmwave_rospkg/launch/6843AOP_odom.py
@@ -8,16 +8,16 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    rviz_arg = DeclareLaunchArgument('rviz', default_value='true', description='Enable RViz')
+    rviz_arg = DeclareLaunchArgument('rviz', default_value='false', description='Enable RViz')
 
     # include IWR6843.py
     package_dir = get_package_share_directory('ti_mmwave_rospkg')
     iwr6843_include = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(package_dir,'launch','IWR6843.py')),
         launch_arguments={
-            "cfg_file": '6843ISK_Occupancy.cfg',
-            "command_port": "/dev/ttyUSB0",
-            "data_port": "/dev/ttyUSB1",
+            "cfg_file": '6843AOP_saberguide.cfg',
+            "command_port": "/dev/ti_command",
+            "data_port": "/dev/ti_data",
             "rviz": LaunchConfiguration('rviz'),
         }.items()
     )
